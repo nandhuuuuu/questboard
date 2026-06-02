f2ca22ca99d2fcf61324506e55559ebf3f2c5073
nandhuuuuu <nandhanasc@gmail.com>
2026-06-02 19:19:31 +0530

    switch to swc

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    react({
-      jsxRuntime: 'classic'
-    }),
+    react(),
     tailwindcss(),
   ],
-  build: {
-    target: 'es2015'
-  }
 })
