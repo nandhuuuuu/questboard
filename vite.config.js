e8ba198c0fdee78555dbaeb9585ae07e0ff9a865
nandhuuuuu <nandhanasc@gmail.com>
2026-06-02 19:32:32 +0530

    fix eval issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: []
+      }
+    }),
     tailwindcss(),
   ],
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    }
+  }
 })
